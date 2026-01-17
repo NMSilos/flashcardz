e850cfe90124e4764482358db0f60dee8a25a364
Nicolas <nicolas.marsil02@gmail.com>
2026-01-17 02:37:58 -0300

    feat: new model User

---
+++ cardz-backend/src/main/java/com/github/nmsilos/cardzbackend/model/User.java
@@ -0,0 +1,45 @@
+package com.github.nmsilos.cardzbackend.model;
+
+import com.github.f4b6a3.uuid.UuidCreator;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue
+    @Column(
+            name = "id",
+            columnDefinition = "BINARY(16)",
+            nullable = false,
+            updatable = false
+    )
+    private UUID id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String email;
+
+    @PrePersist
+    public void prePersist() {
+        if (id == null) {
+            id = UuidCreator.getTimeOrderedEpoch();
+        }
+    }
+}
