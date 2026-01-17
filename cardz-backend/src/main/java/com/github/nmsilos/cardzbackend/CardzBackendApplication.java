e0d6afe5276140060289de948544c3af9575ea23
Nicolas <nicolas.marsil02@gmail.com>
2026-01-17 02:25:22 -0300

    feat: first commit!

---
+++ cardz-backend/src/main/java/com/github/nmsilos/cardzbackend/CardzBackendApplication.java
@@ -0,0 +1,13 @@
+package com.github.nmsilos.cardzbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardzBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardzBackendApplication.class, args);
+    }
+
+}
